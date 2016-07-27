13ae2ad4a8887c939e160082c52e0de4b616dbf1
Basthian Matthews <basthianmatthews@gmail.com>
2016-07-26 22:38:18 -0400

    borrado comentario de flexy
    Signed-off-by: Basthian Matthews <basthianmatthews@gmail.com>

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 
-        <meta name="description" content="FlexyCodes - FlexyCard vCard Template. Creating my personal page!"/>
+        <meta name="description" content="Vcard/Resumen!"/>
 
         <!-- CSS | bootstrap -->
         <!-- Credits: http://getbootstrap.com/ -->
